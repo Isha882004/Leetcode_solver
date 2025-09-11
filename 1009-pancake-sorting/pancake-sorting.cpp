b0e1c31dfdc4fbb19564c223e8f7b2e2d9afc050
Isha Garg <139227761+Isha882004@users.noreply.github.com>
2025-09-11 21:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (74.67%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.cpp
+++ 1009-pancake-sorting/pancake-sorting.cpp
@@ -4,15 +4,14 @@ public:
         int n=arr.size();
         vector<int>res;
         for(int curr=n;curr>1;curr--){
-            int idx=find(arr.begin(),arr.end() ,curr)-arr.begin();
+            int idx= find(arr.begin(),arr.end() ,curr)-arr.begin();
             if(idx==curr-1) continue;
             if(idx!=0){
-              reverse(arr.begin(), arr.begin() + idx + 1);
+                reverse(arr.begin(),arr.begin() + idx+1);
                 res.push_back(idx+1);
             }
-            reverse(arr.begin(),arr.begin()+ curr);
+            reverse(arr.begin(),arr.begin()+curr);
             res.push_back(curr);
-        }
-        return res;
+    }return res;
     }
 };
